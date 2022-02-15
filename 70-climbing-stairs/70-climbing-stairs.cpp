2a1900eed53d2596708ea5542e7bf53f25c1bbb2
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-02-15 18:48:13 +0530

    Time: 0 ms (100.00%), Space: 6 MB (54.32%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -6,14 +6,15 @@ public:
         if(dp[n]!=-1) return dp[n];
         return dp[n] = solve(n-1,dp)+solve(n-2,dp);
     }
-    int climbStairs(int n) {
-        vector<int>dp(n+1,0);
-        dp[0] = 1;
+    int climbStairs(int n) { 
+        int step1 = 1;
+        int step2 = 0;
         for(int i=1;i<=n;i++){
-            int ans = dp[i-1];
-            if(i-2>=0) ans += dp[i-2];
-            dp[i] = ans;
+            int ans = step1;
+            if(i-2>=0) ans += step2;
+            step2 = step1;
+            step1 = ans;
         }
-        return dp[n];
+        return step1;
     }
 };
