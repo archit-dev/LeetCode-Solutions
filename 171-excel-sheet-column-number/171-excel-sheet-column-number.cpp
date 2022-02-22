3378f086ea06290a401a650b003c1aa31b299f2d
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-02-22 15:48:11 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (38.78%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -8,7 +8,8 @@ public:
         int n = columnTitle.size();
         int ans = 0;
         for(int i=0;i<n;i++){
-            ans = ans*26 + findVal(columnTitle[i]);
+            ans*=26 ;
+            ans+= findVal(columnTitle[i]);
         }
         return ans;
     }
