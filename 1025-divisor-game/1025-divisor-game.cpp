8257d4919bc4647f72f7dda915e45a34726aef2b
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-02-26 12:32:17 +0530

    Time: 4 ms (32.58%), Space: 5.9 MB (71.20%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool solve(int n,int currentPlayer){
+        if(n==1 && currentPlayer==0) return false;
+        if(n==1 && currentPlayer==1) return true; 
+        bool ans = false;
+        int newp = currentPlayer == 0 ? 1 : 0 ;
+        ans = solve(n-1,newp);
+        return ans;
+    }
+    
+    bool divisorGame(int n) {
+        //if n==0 and current chance is Alice, its a loss
+        //otherwise its a win 
+        return solve(n,0);
+    }
+};
