a076c754455e3c31b4f96db56ad10d84f124dd7c
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-02-27 22:12:54 +0530

    Time: 20 ms (20.53%), Space: 17.4 MB (48.00%) - LeetHub

---
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    #define ll long long
+    int widthOfBinaryTree(TreeNode* root) {
+        queue<pair<TreeNode*,ll> > q;
+        q.push({root,0});
+        ll ans = INT_MIN;
+        while(!q.empty()){
+            ll n = q.size();
+            ll first,last;
+            ll min_ = q.front().second;
+            for(int i=0;i<n;i++){
+                TreeNode* currNode = q.front().first;
+                ll currIndex = q.front().second-min_;
+                q.pop();
+                if(i==0) first = currIndex;
+                if(i==n-1) last = currIndex;
+                if(currNode->left) q.push({currNode->left,2*currIndex+1}); 
+                if(currNode->right) q.push({currNode->right,2*currIndex+2});
+            }
+            ans = max(ans,last-first+1);
+        }
+        return ans;
+    }
+};
