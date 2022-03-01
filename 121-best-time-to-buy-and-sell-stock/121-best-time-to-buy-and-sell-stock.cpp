5d5e79b8b50b6a8382045876907367b9a174fb32
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-03-01 09:57:47 +0530

    Time: 137 ms (76.26%), Space: 93.4 MB (54.07%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        //the main idea is that, the maximum profit at
+        //any point can be prices[i] - minimumSoFar
         int ans = 0;
         int n = prices.size();
         int minSoFar = INT_MAX;
