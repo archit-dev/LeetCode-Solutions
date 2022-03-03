091a2d072fb32650a88742f242453650ef0132dd
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-03-03 12:57:30 +0530

    Time: 2 ms (42.59%), Space: 5.9 MB (44.87%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -2,13 +2,18 @@ class Solution {
 public:
     double myPow(double x, int n) {
         if(n==0) return 1.0;
-        double temp = myPow(x,n/2);
-        if(n%2==0) return temp*temp;
-        else {
-            if(n > 0)
-                return x*temp*temp;
-            else
-                return (temp*temp)/x;
+        double ans = 1.0;
+        long long nn = n;
+        if(n<0) nn*=-1;
+        while(nn){
+            if(nn%2==0){
+                x*=x;
+                nn/=2;
+            }else{
+                ans*=x;
+                nn-=1;
+            }
         }
+        return n<0 == true ? 1.0/ans : ans;
     }
 };
