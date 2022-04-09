308babf51bc171476e80ce06c00e4b33ff3ec0d0
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-04-09 10:26:50 +0530

    Time: 44 ms (5.16%), Space: 10.5 MB (9.85%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,14 +1,40 @@
 class Solution {
 public:
-    int countNegatives(vector<vector<int>>& grid) {
-        int count = 0;
-        int n = grid.size();
-        int m = grid[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]<0) count++;
+    int getNegativeIndex(vector<int> & row, int low , int high){
+        int ans = high+1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(row[mid]<0){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
             }
         }
-        return count;
+        return ans;
     }
+    
+    int countNegatives(vector<vector<int>>& grid){
+        int n = grid.size();
+        int m = grid[0].size();
+        int count = 0;
+        for(int i=0;i<n;i++){
+            int negativeIndex = getNegativeIndex(grid[i],0,m-1);
+            count += m - negativeIndex;
+        }
+        return count;
+    } 
+    
+    //Naive solution:::---->>>
+    // int countNegatives(vector<vector<int>>& grid) {
+    //     int count = 0;
+    //     int n = grid.size();
+    //     int m = grid[0].size();
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<m;j++){
+    //             if(grid[i][j]<0) count++;
+    //         }
+    //     }
+    //     return count;
+    // }
 };
