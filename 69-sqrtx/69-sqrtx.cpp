52c24845c10783ebc82f687f092d753c983a9854
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-04-09 10:14:59 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (26.52%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -2,10 +2,18 @@ class Solution {
 public:
     int mySqrt(int x) {
         if(x==0 || x==1) return x;
-        int i;
-        for(i=2;(long long)i*i<=x;i++){
-            if(i*i==x) return i;
+        int low = 0, high = x;
+        int ans = 0;
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            if((long long)mid*mid==x) return mid;
+            else if((long long)mid*mid < x){
+                ans = mid;
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
         }
-        return i-1;
+        return ans;
     }
 };
