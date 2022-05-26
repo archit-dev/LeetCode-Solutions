8da1e11fa22a073c02ba6e63978dd3b2229d38de
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-05-26 15:07:02 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -13,8 +13,32 @@ class Solution {
                 if(cycleDFS(nbr,node,graph,visited)) return true;
             }
             else if(visited[nbr]==true && nbr!=parent){
-                //
+                //visited and its not the current node's parent
+                //then there exists a cycle
                 return true;
+            }
+        }
+        return false;
+    }
+    
+    bool cycleBFS(int node,int parent,vector<int> graph[],vector<bool>& visited){
+        visited[node] = true;
+        queue<pair<int,int> > Queue;
+        Queue.push({node,parent});
+        while(!Queue.empty()){
+            int currentNode = Queue.front().first;
+            int currentParent = Queue.front().second;
+            Queue.pop();
+            for(auto nbr : graph[currentNode]){
+                //if neighbour is not visited
+                if(visited[nbr]==false){
+                    Queue.push({nbr,currentNode});
+                    visited[nbr]=true;
+                }else if(visited[nbr]==true && nbr!=currentParent){
+                    //you reach a visited node and 
+                    //it is not the parent of the current node
+                    return true;
+                }
             }
         }
         return false;
@@ -25,7 +49,8 @@ class Solution {
         vector<bool> visited(V,false);
         for(int i=0;i<V;i++){
             if(!visited[i]){
-                if(cycleDFS(i,-1,adj,visited)) return true;
+                // if(cycleDFS(i,-1,adj,visited)) return true;
+                if(cycleBFS(i,-1,adj,visited)) return true;
             }
         }
         return false;
