619c51c218eb7002f3b5a8ed579a29b92a13798b
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-05-27 13:35:31 +0530

    Time: 18 ms (94.33%), Space: 13.7 MB (70.32%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool detectCycleDFS(int node,vector<int>& visitedSoFar,
+                       vector<int>& visitedOnThisPath, vector<int>graph[]){
+        visitedSoFar[node] = 1;
+        visitedOnThisPath[node] = 1;
+        for(auto nbr:graph[node]){
+            if(!visitedSoFar[nbr]){
+                if(detectCycleDFS(nbr,visitedSoFar,visitedOnThisPath,graph)){
+                    return true;
+                }
+            }
+            else if(visitedSoFar[nbr] && visitedOnThisPath[nbr]){
+                return true;
+            }
+        }
+        visitedOnThisPath[node] = 0;
+        return false;
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        //create an adjacency list first
+        vector<int> graph[numCourses];
+        for(int i=0;i<prerequisites.size();i++){
+            int a = prerequisites[i][0];
+            int b = prerequisites[i][1];
+            //a-->b
+            graph[a].push_back(b);
+        }
+        vector<int> visitedSoFar(numCourses,0), visitedOnThisPath(numCourses,0);
+        for(int i=0;i<numCourses;i++){
+            if(!visitedSoFar[i]){
+                if(detectCycleDFS(i,visitedSoFar,visitedOnThisPath,graph)){
+                    // if there is a cycle, then courses cant be scheduled
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
