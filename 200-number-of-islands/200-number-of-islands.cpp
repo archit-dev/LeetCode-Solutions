e83d4bfa1c5f9f4bacf8b862d4db3e4880180c1e
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-05-31 08:30:11 +0530

    Time: 40 ms (75.57%), Space: 18.5 MB (12.72%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isValid(int x,int y,int n,int m){
+        if(x>=0 && x<n && y>=0 && y<m) return true;
+        return false;
+    }
+    
+    void bfsHelper(int cr,int cc, int n,int m,vector<vector<char> > & grid,
+                  vector<vector<bool> > &visited)
+    {
+        visited[cr][cc] = true;
+        queue<pair<int,int> >Queue;
+        Queue.push({cr,cc});
+        int dx[4] = {-1,1,0,0};
+        int dy[4] = {0,0,1,-1};
+        while(!Queue.empty()){
+            int x = Queue.front().first;
+            int y = Queue.front().second;
+            Queue.pop();
+            for(int i=0;i<4;i++){
+                int newX = x+dx[i];
+                int newY = y+dy[i];
+                if(isValid(newX,newY,n,m) && grid[newX][newY]=='1' 
+                   && visited[newX][newY]==false)
+                {
+                    visited[newX][newY] = true;
+                    Queue.push({newX,newY});
+                }
+            }
+        }
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<bool> >visited(n,vector<bool>(m,false));
+        int countOfIslands = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && !visited[i][j]){
+                    bfsHelper(i,j,n,m,grid,visited);
+                    countOfIslands++;
+                }
+            }
+        }
+        return countOfIslands;
+    }
+};
