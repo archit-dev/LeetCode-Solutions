59d458321699fb4c26701d98116516fcf8530f5c
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-05-31 19:42:14 +0530

    Time: 103 ms (5.04%), Space: 15.5 MB (6.43%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
-    void dfsHelper(int node,vector<int>&visited,vector<vector<int>>&isConnected){
-        visited[node] = 1;
-        for(int i=0;i<isConnected.size();i++){
-            int currentNbr = isConnected[node-1][i];
-            if(!visited[i+1] && currentNbr==1){
-                dfsHelper(i+1,visited,isConnected);
+    void bfsHelper(int x,int y,int n,vector<vector<int>>& isConnected,vector<vector<int>>& vis)
+    {
+        vis[x][y] = 1;
+        vis[y][x] = 1;
+        queue<pair<int,int> >Queue;
+        Queue.push({x,y});
+        while(!Queue.empty()){
+            int currX = Queue.front().first;
+            int currY = Queue.front().second;
+            Queue.pop();
+            for(int i=0;i<n;i++){
+                if(vis[currX][i]==0 && isConnected[currX][i]==1){
+                    Queue.push({currX,i});
+                    vis[currX][i] = 1;
+                    vis[i][currX] = 1;
+                }
+            }
+            for(int i=0;i<n;i++){
+                if(vis[currY][i]==0 && isConnected[currY][i]==1){
+                    Queue.push({currY,i});
+                    vis[currY][i] = 1;
+                    vis[i][currY] = 1;
+                }
             }
         }
     }
     
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int>visited(n+1,0);
-        int countOfProvinces=0;
-        for(int i=1;i<=n;i++){
-            if(!visited[i]){
-                dfsHelper(i,visited,isConnected);
-                countOfProvinces++;
+        vector<vector<int> > visited(n,vector<int>(n,0));
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1 && visited[i][j]==0){
+                    bfsHelper(i,j,n,isConnected,visited);
+                    ans++;
+                }
             }
         }
-        return countOfProvinces;
+        return ans;
     }
 };
