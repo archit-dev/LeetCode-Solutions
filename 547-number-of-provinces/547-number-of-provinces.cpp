533c874b8807b6d0edcbafef1ee10662491534af
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-05-31 19:45:07 +0530

    Time: 42 ms (27.01%), Space: 14.2 MB (25.98%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    void bfsHelper(int x,int y,int n,vector<vector<int>>& isConnected,vector<vector<int>>& vis)
+    void bfsHelper(int x,int y,int n,vector<vector<int>>& isConnected,vector<int>&vis)
     {
-        vis[x][y] = 1;
-        vis[y][x] = 1;
+        vis[x] = 1;
+        vis[y] = 1;
         queue<pair<int,int> >Queue;
         Queue.push({x,y});
         while(!Queue.empty()){
@@ -11,17 +11,15 @@ public:
             int currY = Queue.front().second;
             Queue.pop();
             for(int i=0;i<n;i++){
-                if(vis[currX][i]==0 && isConnected[currX][i]==1){
+                if(vis[i]==0 && isConnected[currX][i]==1){
                     Queue.push({currX,i});
-                    vis[currX][i] = 1;
-                    vis[i][currX] = 1;
+                    vis[i] = 1;
                 }
             }
             for(int i=0;i<n;i++){
-                if(vis[currY][i]==0 && isConnected[currY][i]==1){
+                if(vis[i]==0 && isConnected[currY][i]==1){
                     Queue.push({currY,i});
-                    vis[currY][i] = 1;
-                    vis[i][currY] = 1;
+                    vis[i] = 1;
                 }
             }
         }
@@ -29,11 +27,11 @@ public:
     
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<vector<int> > visited(n,vector<int>(n,0));
+        vector<int> visited(n,0);
         int ans = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1 && visited[i][j]==0){
+                if(isConnected[i][j]==1 && visited[i]==0){
                     bfsHelper(i,j,n,isConnected,visited);
                     ans++;
                 }
