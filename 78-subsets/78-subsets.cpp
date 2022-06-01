8f809eb6008a826fc18d8aaaea179f59b65e4064
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-06-01 19:08:29 +0530

    Time: 10 ms (10.93%), Space: 7.7 MB (41.17%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    vector<vector<int> > solve(vector<int>&nums,int idx){
+     vector<vector<int> > solve(vector<int>& nums,int idx){
         if(idx<0){
-            vector<vector<int> > tempAns;
             vector<int>temp;
-            tempAns.push_back(temp);
-            return tempAns;
+            vector<vector<int> > ans;
+            ans.push_back(temp);
+            return ans;
         }
-        vector<vector<int> > remaining = solve(nums,idx-1);
-        vector<vector<int> > newVec;
-        for(int i=0;i<remaining.size();i++){
-            //exclude
-            newVec.push_back(remaining[i]);
-            //include
-            remaining[i].push_back(nums[idx]);
-            newVec.push_back(remaining[i]);
-        }
-        return newVec;
+         vector<vector<int> > rest = solve(nums,idx-1);
+         vector<vector<int> > ans;
+         for(int i=0;i<rest.size();i++){
+             //exclude 
+             ans.push_back(rest[i]);
+             //include
+             rest[i].push_back(nums[idx]);
+             ans.push_back(rest[i]);
+         }
+         return ans;
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
