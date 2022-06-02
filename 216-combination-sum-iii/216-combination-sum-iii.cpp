8d459b07c511482043ea13d1160dbb28c94950fb
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-06-02 09:20:20 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (54.10%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     void helper(int currNum,int k,int target,vector<int>& currentComb,
-               vector<vector<int> >& combinations,vector<bool>& vis)
+               vector<vector<int> >& combinations)
     {
         if(target==0){
             if(k==0)
                 combinations.push_back(currentComb);
             return;
         }
-        if(target<0 || k==0 || vis[currNum] || currNum>9) return;
+        if(target<0 || k==0 || currNum>9) return;
         
         //include
-        vis[currNum] = 1;
         currentComb.push_back(currNum);
-        helper(currNum+1,k-1,target-currNum,currentComb,combinations,vis);
+        helper(currNum+1,k-1,target-currNum,currentComb,combinations);
         
         //exclude
-        vis[currNum] = 0;
         currentComb.pop_back();
-        helper(currNum+1,k,target,currentComb,combinations,vis);
+        helper(currNum+1,k,target,currentComb,combinations);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int>currentComb;
         vector<vector<int> > combinations;
-        vector<bool>vis(10,false);
-        helper(1,k,n,currentComb,combinations,vis);
+        helper(1,k,n,currentComb,combinations);
         return combinations;
     }
 };
