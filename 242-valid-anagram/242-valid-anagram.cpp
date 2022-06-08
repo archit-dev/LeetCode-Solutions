1518f6556e8dc8fce62c3f35f827ddd93793c73a
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-06-08 12:55:50 +0530

    Time: 4 ms (94.55%), Space: 7.4 MB (46.52%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        int alpha[26]={0};
+        for(char x:s){
+            alpha[x-'a']+=1;
+        }
+        for(char x:t){
+            alpha[x-'a']-=1;
+        }
+        for(int i=0;i<26;i++){
+            if(alpha[i]!=0) return false;
+        }
+        return true;
     }
 };
