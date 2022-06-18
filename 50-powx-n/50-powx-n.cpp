898ab14cea05a4acb0060a8b8d6e056497ac5676
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-06-18 11:40:55 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (80.05%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
+    double helper(double x,long long n){
         if(n==0) return 1.0;
         double ans = 1.0;
-        long long nn = n;
-        if(n<0) nn*=-1;
-        while(nn){
-            if(nn%2==0){
-                x*=x;
-                nn/=2;
-            }else{
-                ans*=x;
-                nn-=1;
-            }
+        if(n%2==1){
+            ans *= x;
+            n-=1;
         }
-        return n<0 == true ? 1.0/ans : ans;
+        ans *= helper(x*x,n/2);
+        return ans;
+    }
+    double myPow(double x, int n) {
+        if(n<0) return 1.0/helper(x,-1LL*n);
+        else return helper(x,n);
     }
 };
