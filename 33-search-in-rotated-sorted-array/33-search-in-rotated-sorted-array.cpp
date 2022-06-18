994742febd434f252bb08f85be801a8b28b838b0
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-06-18 13:56:36 +0530

    Time: 7 ms (49.40%), Space: 11.2 MB (29.22%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,40 +1,54 @@
 class Solution {
 public:
-    int getIndexOfPivot(vector<int> & nums){
-        int low = 0 , high = nums.size()-1;
+    int search(vector<int>& nums, int target) {
         int n = nums.size();
+        int minIndex = 0;
+        int low = 0,high = n-1;
         while(low<=high){
             int mid = low + (high-low)/2;
-            int left = (mid + n -1 )%n;
+            int left = (mid-1+n)%n;
             int right = (mid+1)%n;
-            if(nums[low] < nums[high]) return low;
-            if(nums[left]>=nums[mid] && nums[mid] <= nums[right]){
-                return mid;
+            if(nums[low]<=nums[high]){
+                minIndex = low;
+                break;
             }
-            else{
-                if(nums[low] <= nums[mid]) low = mid + 1;
-                else high = mid - 1;
+            if(nums[left]>nums[mid] && nums[mid]<nums[right]){
+                minIndex = mid;
+                break;
+            }else{
+                if(nums[low]<=nums[mid]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
             }
         }
-        return 0;
-    }
-    
-    int binarySearch(vector<int> & nums,int target,int low,int high){
+        //first perform binary search in ascending array
+        low = minIndex, high = n-1;
         while(low<=high){
             int mid = low + (high-low)/2;
             if(nums[mid]==target) return mid;
             else{
-                if(nums[mid]<target) low = mid + 1;
-                else high = mid - 1;
+                if(nums[mid]<target){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+        }
+        //perform binary search in descending array
+        low = 0, high = minIndex-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]==target) return mid;
+            else{
+                if(nums[mid]<target){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
             }
         }
         return -1;
-    }
-    
-    int search(vector<int>& nums, int target) {
-        int idxPivot = getIndexOfPivot(nums); 
-        int leftSearch = binarySearch(nums,target,0,idxPivot-1);
-        if(leftSearch==-1) return binarySearch(nums,target,idxPivot,nums.size()-1);
-        return leftSearch;
     }
 };
