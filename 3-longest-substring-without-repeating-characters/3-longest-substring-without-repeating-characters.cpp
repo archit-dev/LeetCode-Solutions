5f0eaa618529c10abffddeb06f733827f0d15746
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-06-19 15:53:30 +0530

    Time: 32 ms (41.58%), Space: 10.7 MB (27.70%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char,int> mp;
+        int low = 0, high = 0;
+        int n = s.size();
         int ans = 0;
-        int ptr1 = 0, ptr2 = 0;
-        while(ptr2<s.size()){
-            //keep acquiring until it becomes invalid
-            char current = s[ptr2];
-            if(mp.find(current)==mp.end()){
-                //not found yet
-                mp[current]+=1;
+        unordered_map<char,int> alpha;
+        while(high<n){
+            if(alpha.find(s[high])==alpha.end()){
+                //new character
             }else{
-                //this character has been found before
-                //so now keep removing from the start
-                //until the hashmap becomes valid again
-                mp[current]+=1;
-                while(ptr1<s.size() && mp[current]!=1){
-                    mp[s[ptr1]]-=1;
-                    ptr1++;
+                //old character
+                while(low <=high && alpha.find(s[high])!=alpha.end()){
+                    alpha[s[low]]-=1;
+                    if(alpha[s[low]]==0){
+                        alpha.erase(s[low]);
+                    }
+                    low++;
                 }
             }
-            ans = max(ans,ptr2-ptr1+1);
-            ptr2++;
+            alpha[s[high]]+=1;
+            ans = max(ans,high-low+1);
+            high++;
         }
         return ans;
     }
