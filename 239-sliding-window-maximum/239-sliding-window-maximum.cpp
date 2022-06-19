5ee7255f5bdc3259896500aa041545079c4a76be
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-06-19 12:56:55 +0530

    Time: 438 ms (49.63%), Space: 136.9 MB (35.57%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int>dq;
-        vector<int>ans;
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            if(dq.empty()==false && dq.front()==i-k) dq.pop_front();
-            
-            int currentNum = nums[i];
-            //pop all the smaller elements from the back of the que
-            while(dq.empty()==false && nums[dq.back()]<currentNum){
+        deque<pair<int,int> >dq;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(!dq.empty() && dq.front().second==i-k){
+                dq.pop_front();
+            }
+            while(!dq.empty() && dq.back().first<=nums[i]){
                 dq.pop_back();
             }
-            dq.push_back(i);
+            dq.push_back({nums[i],i});
             if(i>=k-1){
-                ans.push_back(nums[dq.front()]);
+                ans.push_back(dq.front().first);
             }
         }
         return ans;
