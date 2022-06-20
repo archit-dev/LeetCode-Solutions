e9252259bf226759b241339f592fb68ce0559053
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-06-21 04:08:41 +0530

    Time: 50 ms (28.00%), Space: 16.6 MB (10.76%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int currentMatch = 0 , reqMatch = t.size();
-        unordered_map<char,int> countT,countS;
-        for(char x:t) countT[x]+=1;
-        int low = 0, high = 0, bestLen = INT_MAX;
+        unordered_map<char,int>mp1,mp2;
+        for(char x:t) mp2[x]+=1;
+        int reqMatch = t.size() , currMatch = 0;
+        int low = 0, high = 0 , bestLen = INT_MAX;
         string ans = "";
         while(high<s.size()){
-            char curr = s[high];
-            if(countT.find(curr)!=countT.end()){
-                //this character is from string T
-                countS[curr]+=1;
-                if(countS[curr]<=countT[curr]){
-                    currentMatch+=1;
+            if(mp2.find(s[high])!=mp2.end()){
+                mp1[s[high]]+=1;
+                if(mp1[s[high]]<=mp2[s[high]]){
+                    currMatch+=1;
                 }
-            }
-            while(currentMatch==reqMatch){
-                //store potential ans
-                if(high-low+1<bestLen){
-                    bestLen = high-low+1;
-                    ans = s.substr(low,bestLen);
-                }
-                
-                //remove the left char
-                if(countT.find(s[low])!=countT.end()){
-                    countS[s[low]]-=1;
-                    if(countS[s[low]]<countT[s[low]]){
-                        currentMatch-=1;
+                while(currMatch==reqMatch){
+                    if(bestLen>high-low+1){
+                        bestLen = high-low+1;
+                        ans = s.substr(low,bestLen);
                     }
+                    if(mp2.find(s[low])!=mp2.end()){
+                        mp1[s[low]]-=1;
+                        if(mp1[s[low]]<mp2[s[low]]){
+                            currMatch-=1;
+                        }
+                    }
+                    low++;
                 }
-                low++;
             }
             high++;
         }
