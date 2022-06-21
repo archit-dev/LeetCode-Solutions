15fb40ada299a6a296703dbfe0de7b1a49f91552
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-06-21 21:12:21 +0530

    Time: 19 ms (26.19%), Space: 17.3 MB (52.54%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -1,36 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    #define ll unsigned long long
     int widthOfBinaryTree(TreeNode* root) {
-        queue<pair<TreeNode*,ll> > q;
+        if(!root) return 0;
+        long long width = 1;
+        queue<pair<TreeNode * , long long> >q;
         q.push({root,0});
-        ll ans = 0;
         while(!q.empty()){
-            ll n = q.size();
-            ll first,last;
-            ll min_ = q.front().second;
-            for(int i=0;i<n;i++){
-                TreeNode* currNode = q.front().first;
-                ll currIndex = q.front().second;
+            long long n = q.size();
+            long long minIdx = q.front().second;
+            long long low = 0, high = 0;
+            for(long long i=0;i<n;i++){
+                TreeNode * node = q.front().first;
+                long long currIdx = q.front().second-minIdx;
                 q.pop();
-                if(i==0) first = currIndex;
-                if(i==n-1) last = currIndex;
-                if(currNode->left) q.push({currNode->left,2*currIndex+1}); 
-                if(currNode->right) q.push({currNode->right,2*currIndex+2});
+                if(i==0) low = currIdx;
+                if(i==n-1) high = currIdx;
+                
+                if(node->left){
+                    q.push({node->left,2*currIdx+1});
+                }
+                if(node->right){
+                    q.push({node->right,2*currIdx+2});
+                }
             }
-            ans = max(ans,last-first+1);
+            width = max(high-low+1,width);
         }
-        return ans;
+        return width;
     }
 };
