2bf17650a72fc1faf852307119bc4a36adccc224
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-06-21 20:11:37 +0530

    Time: 11 ms (83.92%), Space: 21.3 MB (74.40%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -1,21 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-public:
+    private:
+    bool isLeaf(TreeNode* root){
+        return !root->left && !root->right;
+    }
+    public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(!root) return false;
-        int currentVal = root->val;
-        targetSum-=currentVal;
-        if(targetSum==0 && !root->left && !root->right) return true;
-        return hasPathSum(root->left,targetSum) || hasPathSum(root->right,targetSum);
+        if(isLeaf(root)){
+            if(targetSum==root->val) return true;
+            else return false;
+        }
+        return hasPathSum(root->left,targetSum-root->val) ||
+                hasPathSum(root->right,targetSum-root->val);
     }
 };
