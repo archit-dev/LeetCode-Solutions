951dc010b2ded8c55fcb2a1abe065c8999e73620
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-06-22 12:33:26 +0530

    Time: 6 ms (56.96%), Space: 13 MB (75.51%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.cpp
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -1,32 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void pathsHelper(TreeNode * root, vector<string>&paths, string currentPath){
-        if(root->left==nullptr && root->right==nullptr){
-            if(currentPath.size()!=0) currentPath+= "->";
-            currentPath+=to_string(root->val);
-            paths.push_back(currentPath);
+    bool isLeaf(TreeNode * root){
+        return !root->left && !root->right;
+    }
+    void helper(TreeNode * root,vector<string>& ans,string currPath){
+        if(isLeaf(root)){
+            currPath+=to_string(root->val);
+            ans.push_back(currPath);
             return ;
         }
-        if(currentPath.size()==0) currentPath = to_string(root->val);
-        else currentPath+= "->" + to_string(root->val);
-        if(root->left) pathsHelper(root->left,paths,currentPath);
-        if(root->right) pathsHelper(root->right,paths,currentPath);
+        currPath+=to_string(root->val)+"->";
+        if(root->left)helper(root->left,ans,currPath);
+        if(root->right)helper(root->right,ans,currPath);
     }
-    
     vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string> paths;
-        pathsHelper(root,paths,"");
-        return paths;
+        vector<string>ans;
+        if(!root) return ans;
+        helper(root,ans,"");
+        return ans;
     }
 };
