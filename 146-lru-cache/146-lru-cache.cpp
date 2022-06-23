305126927a744918cd954257c7c25b6b6565bebe
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-06-23 10:34:53 +0530

    Time: 765 ms (18.63%), Space: 172.5 MB (53.94%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,22 +1,21 @@
-class LRUCache { 
+class LRUCache {
 public:
-    class LRUNode{
-        public:
-            int key,value;
-            LRUNode* next, *prev;
+    struct Node{
+        int keyy,val;
+        Node*next,*prev;
         
-            LRUNode(int key_,int val_){
-                key = key_;
-                value = val_;
-                next = nullptr;
-                prev = nullptr;
-            }
+        Node(int key_,int val_){
+            keyy = key_;
+            val = val_;
+            next=NULL;
+            prev=NULL;
+        }
     };
     
-    unordered_map<int,LRUNode*> mp;
-    LRUNode * head = new LRUNode(-1,-1);
-    LRUNode * tail = new LRUNode(-1,-1);
+    Node * head = new Node(-1,-1);
+    Node * tail = new Node(-1,-1);
     int capa;
+    unordered_map<int,Node*> nodeMap;
     
     LRUCache(int capacity) {
         capa = capacity;
@@ -24,53 +23,50 @@ public:
         tail->prev = head;
     }
     
-    void addNode(LRUNode* newNode){
-        LRUNode* temp = head->next;
-        head->next = newNode;
-        newNode->next = temp;
-        newNode->prev = head;
-        temp->prev = newNode; 
-    }
-    
-    void deleteNode(LRUNode* reqNode){
-        LRUNode * prevNode = reqNode->prev;
-        LRUNode * nextNode = reqNode->next;
+    void deleteNode(Node * req){
+        Node * prevNode = req->prev;
+        Node * nextNode = req->next;
         prevNode->next = nextNode;
         nextNode->prev = prevNode;
-        // delete reqNode; 
     }
-
-    int get(int key_) {
-        //if not found
-        if(mp.find(key_)==mp.end()) return -1;
+    
+    void insertAtHead(Node * newNode){
+        Node * nextNode = head->next;
+        newNode->next = nextNode;
+        newNode->prev = head;
+        nextNode->prev = newNode;
+        head->next = newNode;
+    }
+    
+    int get(int key) {
+        if(nodeMap.find(key)==nodeMap.end())return -1;
         
-        //if found
-        LRUNode * reqNode = mp[key_];
-        int val = reqNode->value;
+        Node * reqNode = nodeMap[key];
+        int reqValue = reqNode->val;
         deleteNode(reqNode);
-        mp.erase(key_);
-        addNode(reqNode);
-        mp[key_] = head->next;
-        return val;
+        insertAtHead(new Node(key,reqValue));
+        nodeMap[key] = head->next;
+        return reqValue;
     }
-
-    void put(int key_, int value) {
-        if(mp.find(key_)!=mp.end()){
-            //this node already exists
-            //remove this node
-            LRUNode * reqNode = mp[key_];
-            mp.erase(key_);
+    
+    void put(int key, int value) {
+        if(nodeMap.find(key)!=nodeMap.end()){
+            //this node exists before
+            Node * reqNode = nodeMap[key];
             deleteNode(reqNode);
+            insertAtHead(new Node(key,value));
+            nodeMap[key] = head->next;
+            return;
         }
-        if(mp.size()==capa){
-            //first delete the last node and then insert
-            int temp = tail->prev->key;
+        
+        if(nodeMap.size() == capa){
+            //first remove the last node
+            nodeMap.erase(tail->prev->keyy);
             deleteNode(tail->prev);
-            mp.erase(temp);
         }
-        //add the node
-        LRUNode* newNode = new LRUNode(key_,value);
-        addNode(newNode);
-        mp[key_] = head->next;
+        
+        insertAtHead(new Node(key,value));
+        nodeMap[key] = head->next;
+        return;
     }
 };
