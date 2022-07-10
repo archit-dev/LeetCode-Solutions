70c472731cfbe99af035f39e70008864f738188d
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-07-10 16:40:40 +0530

    Time: 17 ms (18.02%), Space: 13.7 MB (51.95%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    int solve(vector<int>& cost,int curr,int n,vector<int>&dp){
-        if(curr==n) return 0;
-        if(curr>n) return 1e9;
-        if(dp[curr+1]!=-1) return dp[curr+1];
-         
-        int currCost = curr==-1 ? 0 : cost[curr];
-        int oneStep = solve(cost,curr+1,n,dp);
-        int twoSteps = solve(cost,curr+2,n,dp);
-        return dp[curr+1] = min(oneStep,twoSteps) + currCost;
+    int dp[10001];
+    int solve(int currIndex,vector<int>& cost){
+        if(currIndex==cost.size()){
+            return 0;
+        }
+        if(currIndex>cost.size()){
+            return 1e9;
+        }
+        if(dp[currIndex]!=-1) return dp[currIndex];
+        int one = cost[currIndex]+solve(currIndex+1,cost);
+        int two = cost[currIndex]+solve(currIndex+2,cost);
+        return dp[currIndex] = min(one,two);
     }
     
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int>dp(n+1,-1);
-        return solve(cost,-1,n,dp);
-
+        memset(dp,-1,sizeof dp);
+        int zero = solve(0,cost);
+        int one = solve(1,cost);
+        return min(one,zero);
     }
 };
