140af496f857569d9c55b1f49be129255a463e44
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-07-19 01:19:30 +0530

    Time: 14 ms (94.70%), Space: 15.4 MB (5.80%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    #define pii pair<int,int>
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> mpp;
-        for(int x: nums) mpp[x]++;
-        priority_queue<pii> pq;
-        for(auto &it: mpp){
-            pq.push({it.second,it.first});
+        unordered_map<int,int>mp;
+        int n = nums.size();
+        for(int x:nums) mp[x]+=1;
+        vector<vector<int>>temp(n+1);
+        for(auto it:mp){
+            int num = it.first;
+            int cnt = it.second;
+            temp[cnt].push_back(num);
         }
         vector<int> ans;
-        while(k--){
-            ans.push_back(pq.top().second);
-            pq.pop();
+        int ptr = n;
+        while(ptr>=0 && ans.size()!=k){
+            for(int x:temp[ptr]){
+                ans.push_back(x);
+                if(ans.size()==k) break;
+            }
+            ptr--;
         }
         return ans;
     }
