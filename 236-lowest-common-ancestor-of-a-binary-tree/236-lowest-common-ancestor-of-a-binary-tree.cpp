f85e4e93a7b72f05a05f7a67de123d95a3722841
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-07-19 08:41:19 +0530

    Time: 16 ms (89.68%), Space: 14.3 MB (25.30%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,12 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root || root==p || root==q) return root;
-        TreeNode * left_ = lowestCommonAncestor(root->left,p,q);
-        TreeNode * right_ = lowestCommonAncestor(root->right,p,q);
-        if(!left_ && right_) return right_;
-        if(left_ && !right_) return left_;
-        if(!left_ && !right_) return nullptr;
+        if(!root || p==root || q==root) return root;
+        TreeNode * leftAns = lowestCommonAncestor(root->left,p,q);
+        TreeNode * rightAns = lowestCommonAncestor(root->right,p,q);
+        if(leftAns && !rightAns) return leftAns;
+        if(!leftAns && rightAns) return rightAns;
+        if(!leftAns && !rightAns) return nullptr;
         return root;
     }
 };
