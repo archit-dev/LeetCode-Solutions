717dcb1e7fd0aeff236dd111e4899daf1391502e
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-07-19 20:35:22 +0530

    Time: 4 ms (81.41%), Space: 8.1 MB (71.72%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    int dp[501][501];
-    
-    int helper(vector<int>& arr,int ptr,int time){
-        if(ptr==arr.size()){
-            return 0;
-        }
-        if(dp[ptr][time] != -1) return dp[ptr][time];
-        
-        //exclude
-        int exclude = helper(arr,ptr+1,time);
-        //include
-        int include = helper(arr,ptr+1,time+1)+(arr[ptr]*time);
-        
-        return dp[ptr][time] = max(exclude,include);
-    }
-    
     int maxSatisfaction(vector<int>& satisfaction) {
-        memset(dp,-1,sizeof dp);
         sort(satisfaction.begin(),satisfaction.end());
-        return helper(satisfaction,0,1);
+        int n = satisfaction.size();
+        int cumSum = 0, ans = 0;
+        for(int i=n-1;i>=0;i--){
+            cumSum+=satisfaction[i];
+            if(cumSum+ans<ans){
+                break;
+            }
+            ans+=cumSum;
+        }
+        return ans;
     }
 };
