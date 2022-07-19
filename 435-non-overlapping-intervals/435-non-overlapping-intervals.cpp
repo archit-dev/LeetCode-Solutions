02221d1b10e2674cf658d55e9b418d35e59e33f5
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-07-19 07:58:16 +0530

    Time: 604 ms (65.13%), Space: 89.9 MB (47.12%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    static bool cmp(vector<int> &a,vector<int>&b){
+    static bool cmp(vector<int>& a,vector<int>& b){
         if(a[0]!=b[0]) return a[0]<b[0];
         return a[1]<b[1];
     }
+    
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end(),cmp);
         int count = 0;
