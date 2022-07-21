86d0a29f97d4703ef4a9089337819bf42408c3ac
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-07-21 08:04:46 +0530

    Time: 43 ms (34.35%), Space: 22.2 MB (5.43%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -3,7 +3,7 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         unordered_map<int,unordered_set<int>>rows;
         unordered_map<int,unordered_set<int>>cols;
-        map<pair<int,int>,unordered_set<int>>squares;
+        unordered_map<int,unordered_set<int>>squares;
         
         for(int r=0;r<9;r++){
             for(int c=0;c<9;c++){
@@ -14,14 +14,14 @@ public:
                 char curr = board[r][c];
                 if(rows[r].find(curr)!=rows[r].end() ||
                    cols[c].find(curr)!=cols[c].end() || 
-                   squares[{r/3,c/3}].find(curr)!=squares[{r/3,c/3}].end())
+                   squares[r/3*3+c/3].find(curr)!=squares[r/3*3+c/3].end())
                 {
                     return false;
                 }
                 
                 rows[r].insert(curr);
                 cols[c].insert(curr);
-                squares[{r/3,c/3}].insert(curr);
+                squares[r/3*3+c/3].insert(curr);
             }
         }
         
