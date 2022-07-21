2a2a61b7c36cce3c2b87950fc007505d73ab5322
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-07-21 18:44:08 +0530

    Time: 2 ms (66.71%), Space: 6 MB (57.98%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
-    int findVal(char x){
-        return x-'A'+1;
-    }
-    
     int titleToNumber(string columnTitle) {
-        int n = columnTitle.size();
         int ans = 0;
-        for(int i=0;i<n;i++){
-            ans*=26 ;
-            ans+= findVal(columnTitle[i]);
+        for(char x:columnTitle){
+            ans = ans*26+ (x-'A'+1);
         }
         return ans;
     }
