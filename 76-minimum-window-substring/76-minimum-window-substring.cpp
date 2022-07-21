4d17032ba9fdad48916b2ffc5811cdbfa0c50eb3
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-07-21 19:45:08 +0530

    Time: 37 ms (42.57%), Space: 16.4 MB (15.92%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -2,31 +2,32 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         unordered_map<char,int>mp1,mp2;
-        for(char x:t) mp2[x]+=1;
-        int reqMatch = t.size() , currMatch = 0;
-        int low = 0, high = 0 , bestLen = INT_MAX;
+        for(char x:t){
+            mp1[x]+=1;
+        }
+        int requiredMatch = t.size(), matches = 0;
+        
+        int low = 0 , high = 0, n = s.size(), bestLen = INT_MAX;
         string ans = "";
-        while(high<s.size()){
-            if(mp2.find(s[high])!=mp2.end()){
-                mp1[s[high]]+=1;
-                if(mp1[s[high]]<=mp2[s[high]]){
-                    currMatch+=1;
-                }
-                while(currMatch==reqMatch){
+        while(high<n){
+            char curr = s[high];
+            if(mp1.find(curr)!=mp1.end()){
+                mp2[curr]+=1;
+                if(mp2[curr]<=mp1[curr]) matches+=1;
+                while(matches==requiredMatch){
                     if(bestLen>high-low+1){
                         bestLen = high-low+1;
                         ans = s.substr(low,bestLen);
                     }
-                    if(mp2.find(s[low])!=mp2.end()){
-                        mp1[s[low]]-=1;
-                        if(mp1[s[low]]<mp2[s[low]]){
-                            currMatch-=1;
-                        }
+                    if(mp1.find(s[low])!=mp1.end()){
+                        //this is a valid char from second str
+                        mp2[s[low]]-=1;
+                        if(mp2[s[low]]<mp1[s[low]]) matches-=1;
                     }
-                    low++;
+                    low+=1;
                 }
             }
-            high++;
+            high+=1;
         }
         return ans;
     }
