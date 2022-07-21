e1ce98387189fada8e0f099376a4891099b98823
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-07-21 19:53:51 +0530

    Time: 420 ms (63.84%), Space: 134.6 MB (85.05%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<pair<int,int> >dq;
-        vector<int> ans;
+        deque<int>dq;
+        vector<int>ans;
         for(int i=0;i<nums.size();i++){
-            if(!dq.empty() && dq.front().second==i-k){
+            if(!dq.empty() && i-k==dq.front()){
                 dq.pop_front();
             }
-            while(!dq.empty() && dq.back().first<=nums[i]){
+            
+            while(!dq.empty() && nums[i]>=nums[dq.back()]){
                 dq.pop_back();
             }
-            dq.push_back({nums[i],i});
+            dq.push_back(i);
+            
             if(i>=k-1){
-                ans.push_back(dq.front().first);
+                ans.push_back(nums[dq.front()]);
             }
         }
+        
         return ans;
     }
 };
