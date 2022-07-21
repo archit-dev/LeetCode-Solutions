d1cd54e30632b8ce2301765612680302463d0720
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-07-21 20:28:32 +0530

    Time: 134 ms (5.54%), Space: 19 MB (28.18%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    bool canEat(vector<int> piles,int k,int h){
-        int time = 0, ptr = 0, n = piles.size();
-        for(int i=0;i<n;i++){
-            time+=piles[i]/k;
-            if(piles[i]%k!=0)time+=1;
+    bool canEat(vector<int>& piles,int h,int k){
+        int time = 0;
+        for(int i=0;i<piles.size();i++){
+            time += piles[i]/k;
+            if(piles[i]%k!=0) time+=1;
             if(time>h) return false;
         }
         return true;
     }
+    
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1;
-        int high = *max_element(piles.begin(),piles.end()); 
+        int low = 1, high = *max_element(piles.begin(),piles.end());
         int ans = high;
         while(low<=high){
             int mid = low + (high-low)/2;
-            if(canEat(piles,mid,h)){
+            if(canEat(piles,h,mid)){
                 ans = mid;
                 high = mid-1;
             }else{
