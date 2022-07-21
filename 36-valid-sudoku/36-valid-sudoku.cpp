47fe77a49f8313ed01d888d8af972fc276b45d71
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-07-21 07:50:04 +0530

    Time: 44 ms (32.02%), Space: 22.4 MB (5.43%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,52 +1,30 @@
 class Solution {
 public:
-    bool checkRow(vector<vector<char>>& board,int row){
-        unordered_map<char,int> mp;
-        for(int i=0;i<9;i++){
-            if(board[row][i]!='.') mp[board[row][i]]+=1;
-        }
-        for(auto it:mp){
-            if(it.second>1) return false;
-        }
-        return true;
-    }
-    
-    bool checkCol(vector<vector<char>>& board,int col){
-        unordered_map<char,int> mp;
-        for(int i=0;i<9;i++){
-            if(board[i][col]!='.') mp[board[i][col]]+=1;
-        }
-        for(auto it:mp){
-            if(it.second>1) return false;
-        }
-        return true;
-    }
-    
-    bool checkBox(vector<vector<char>>& board,int row,int col){
-        unordered_map<char,int> mp;
-        for(int i=row;i<row+3;i++){
-            for(int j=col;j<col+3;j++){
-                if(board[i][j]!='.') mp[board[i][j]]+=1;
-            }
-        }
-        for(auto it:mp){
-            if(it.second>1) return false;
-        }
-        return true;
-    }
-        
     bool isValidSudoku(vector<vector<char>>& board) {
-        for(int i=0;i<9;i++){
-            if(!checkRow(board,i)) return false;
-            for(int j=0;j<9;j++){
-                if(!checkCol(board,j)) return false;
+        unordered_map<int,unordered_set<int>>rows;
+        unordered_map<int,unordered_set<int>>cols;
+        map<pair<int,int>,unordered_set<int>>squares;
+        
+        for(int r=0;r<9;r++){
+            for(int c=0;c<9;c++){
+                if(board[r][c]=='.'){
+                    continue;
+                }
+                
+                char curr = board[r][c];
+                if(rows[r].find(curr)!=rows[r].end() ||
+                   cols[c].find(curr)!=cols[c].end() || 
+                   squares[{r/3,c/3}].find(curr)!=squares[{r/3,c/3}].end())
+                {
+                    return false;
+                }
+                
+                rows[r].insert(curr);
+                cols[c].insert(curr);
+                squares[{r/3,c/3}].insert(curr);
             }
         }
-        int dx[9] = {0,0,0,3,3,3,6,6,6};
-        int dy[9] = {0,3,6,0,3,6,0,3,6};
-        for(int i=0;i<9;i++){
-            if(!checkBox(board,dx[i],dy[i])) return false;
-        }
+        
         return true;
     }
 };
