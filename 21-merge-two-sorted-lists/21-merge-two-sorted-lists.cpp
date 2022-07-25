d7b08a105f65d00c65ca724a7a7a532aa5e24016
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-07-25 09:15:57 +0530

    Time: 4 ms (94.66%), Space: 14.7 MB (81.00%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -1,31 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode();
-        ListNode * curr = dummy;
-        while(list1 && list2){
-            if(list1->val < list2->val){
-                curr->next = list1;
-                list1=list1->next;
-                curr=curr->next;
-            }else{
-                curr->next = list2;
-                list2=list2->next;
-                curr=curr->next;
+        ListNode * ptr1 = list1 , *ptr2 = list2;
+        
+        ListNode * tempHead = new ListNode(-1);
+        ListNode * dummy = tempHead;
+        
+        while(ptr1 && ptr2){
+            if(ptr1->val < ptr2->val){
+                dummy->next = ptr1;
+                ptr1=ptr1->next;
             }
+            else{
+                dummy->next = ptr2;
+                ptr2=ptr2->next;
+            }
+            dummy = dummy->next;
         }
-        if(list1) curr->next = list1;
-        if(list2) curr->next = list2;
-        return dummy->next;
+        
+        if(ptr1) dummy->next = ptr1;
+        if(ptr2) dummy->next = ptr2;
+        
+        return tempHead->next;
     }
 };
