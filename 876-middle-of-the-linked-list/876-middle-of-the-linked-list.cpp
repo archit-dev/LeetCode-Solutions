74970edf60750de89c5fd6ece212cf905c898d88
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-07-25 09:36:44 +0530

    Time: 0 ms (100.00%), Space: 7 MB (71.58%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -11,13 +11,14 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        if(!head) return nullptr;
-        ListNode * slow = head;
-        ListNode * fast = head;
+        if(!head || !head->next) return head;
+        ListNode* slow = head, *fast = head;
+        
         while(fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
         }
+        
         return slow;
     }
 };
