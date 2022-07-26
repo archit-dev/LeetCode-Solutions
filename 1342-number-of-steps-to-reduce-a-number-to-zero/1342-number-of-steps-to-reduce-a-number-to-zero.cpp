c2669d83b185a028855e0aa29c1416794f134a66
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-07-27 00:04:49 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.87%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -3,12 +3,9 @@ public:
     int numberOfSteps(int num) {
         int steps = 0;
         while(num){
-            if((num&1)==0){
-                num/=2;
-            }else{
-                num-=1;
-            }
-            steps++;
+            if(num%2==0) num/=2;
+            else num-=1;
+            steps+=1;
         }
         return steps;
     }
