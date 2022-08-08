21eac254f4ae4cba097067a0c08b8465ce37a0d9
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2022-08-08 21:03:50 +0530

    Time: 14 ms (90.27%), Space: 10.6 MB (88.08%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    int helper(string &a,int alen,string &b,int blen,vector<vector<int>>&dp){
-        if(alen<0 || blen<0) return 0;
+    int longestCommonSubsequence(string x, string y) {
+        int n=x.size(),m=y.size();
+        int dp[n+1][m+1];
         
-        if(dp[alen][blen]!=-1) return dp[alen][blen];
+        for(int i=0;i<=n;i++)dp[i][0]=0;
+        for(int i=0;i<=m;i++)dp[0][i]=0;
         
-        if(a[alen]==b[blen]) return dp[alen][blen] = 1+helper(a,alen-1,b,blen-1,dp);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(x[i-1]==y[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
         
-        int includeA = helper(a,alen,b,blen-1,dp);
-        int includeB = helper(a,alen-1,b,blen,dp);
-        return dp[alen][blen] = max(includeA,includeB);
+        return dp[n][m];
     }
-    
-    int longestCommonSubsequence(string text1, string text2) {
-        int n1 = text1.size() , n2 = text2.size();
-        vector<vector<int>>dp(n1,vector<int>(n2,-1));
-        return helper(text1,n1-1,text2,n2-1,dp);
-    }
+
 };
